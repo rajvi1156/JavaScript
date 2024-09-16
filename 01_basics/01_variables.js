1d295267b6e00ef6dc09a5d3df1ba16d3890ece6
rajvi1156 <rajvisoneji1156@gmail.com>
2024-09-16 15:23:32 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553 
+let accountEmail = "rajvisoneji1156@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+accountEmail = "abc@gmail.com"
+accountPassword = "212121"
+accountCity = "deesa"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(2 +3) // we are using nodejs ,not browser 
+
+// code readability should br hihg 
+
+let name = "rajvi"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// BigInt
+// String => ""
+// boolean => true / false
+// null =>standalon value 
+// undefined => 
+// Symbol => unique 
+
+// Object => 
+
+    console.log(typeof "rajvi"); // string
+    console.log(typeof undefined); // undefined 
+    console.log(typeof null); // object
+    
+    
+
+    
